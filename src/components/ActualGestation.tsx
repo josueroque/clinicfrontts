e72350bb5b1d85d180ca8f0462d887752fb9dd49
Josue Roque <jroque@applaudstudios.com>
2021-08-15 17:26:19 -0600

    Configured stepper to can move freely to any step

--- src/components/ActualGestation.tsx
+++ src/components/ActualGestation.tsx
@@ -7,6 +7,7 @@ import {MuiPickersUtilsProvider,KeyboardDatePicker} from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import Stepper from '@material-ui/core/Stepper';
+import StepButton from '@material-ui/core/StepButton';
 import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
 import StepContent from '@material-ui/core/StepContent';
@@ -33,11 +34,19 @@ createStyles({
   resetContainer: {
     padding: theme.spacing(3),
   },
+  instructions: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+  },
+  completed: {
+    display: 'inline-block',
+  },
 }),
 );
 
 function getSteps() {
-  return ['General Data', 'Antitetanic', 'Visual Inspeccion','Cervix', 'Group'];
+  return ['General Data', 'Antitetanic', 'Visual Inspeccion','Cervix', 'Group'
+          ,'Toxoplasmosis', 'VIH', 'Hemoglobin', 'Syphilis',  'Bacteriruria', 'Blood Glocose' ];
 }
 
 
@@ -50,6 +59,8 @@ const ActualGestation = (props:any)=>{
   const [lastMenstruationDate, updateLastMenstruationDate] = useState<Date|null>(new Date());
   const [size, updateSize] = useState<number>(0);
   const [previousWeight, updatePreviousWeight] = useState<number>(0);
+  const [completed, setCompleted] = React.useState(new Set<number>());
+  const [skipped, setSkipped] = React.useState(new Set<number>());
   
   const classes = useStyles();
   const steps = getSteps();
@@ -77,7 +88,7 @@ const ActualGestation = (props:any)=>{
       switch (step) {
         case 0:
           return (
-            <Container>
+            <Container className='StepContent'>
               <FormControl > 
                 <MuiPickersUtilsProvider utils={DateFnsUtils}>
                   <Grid container justify ="flex-start">
@@ -161,57 +172,107 @@ const ActualGestation = (props:any)=>{
     }  
 
   
-    const handleNext = () => {
-      setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    const totalSteps = () => {
+      return steps.length;
     };
   
-    const handleBack = () => {
-      setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    const completedSteps = () => {
+      return Object.keys(completed).length;
     };
+
+    const isLastStep = () => {
+      return activeStep === totalSteps() - 1;
+    };
+  
+    const allStepsCompleted = () => {
+      return completedSteps() === totalSteps();
+    };
+
+    const handleStep = (step: number) => () => {
+      setActiveStep(step);
+    };
+
+    const skippedSteps = () => {
+      return skipped.size;
+    };
+
+    const handleNext = () => {
+      const newActiveStep =
+        isLastStep() && !allStepsCompleted()
+          ? // It's the last step, but not all steps have been completed,
+            // find the first step that has been completed
+            steps.findIndex((step, i) => !(i in completed))
+          : activeStep + 1;
+      setActiveStep(newActiveStep);
+    };
+  
+    const handleComplete = () => {
+      const newCompleted = new Set(completed);
+      newCompleted.add(activeStep);
+      setCompleted(newCompleted);
+  
+      /**
+       * Sigh... it would be much nicer to replace the following if conditional with
+       * `if (!this.allStepsComplete())` however state is not set when we do this,
+       * thus we have to resort to not being very DRY.
+       */
+      if (completed.size !== totalSteps() - skippedSteps()) {
+        handleNext();
+      }
+    };    
   
     const handleReset = () => {
       setActiveStep(0);
     };
-  
+    
+    const isStepOptional = (step: number) => {
+      return step === 1;
+    };
+
+    const isStepSkipped = (step: number) => {
+      return skipped.has(step);
+    };
 
   return ( 
-    <Fragment>
-      <Sidebar></Sidebar>
-      <h1 className="HistoryTitle"> 
-          {`Current Gestation ${patient?.name} ${patient?.lastName}`  }
-      </h1>
-      <div className={classes.root}>
-        <Stepper activeStep={activeStep} orientation="vertical">
-          {steps.map((label, index) => (
+        <Fragment>
+          <Sidebar></Sidebar>
+          <h1 className="HistoryTitle"> 
+              {`Current Gestation ${patient?.name} ${patient?.lastName}`  }
+          </h1>
+          <div className={classes.root}>
+        <Stepper nonLinear activeStep={activeStep} orientation="vertical"  >
+          {steps.map((label, index) => {
+          const stepProps: { completed?: boolean } = {};
+          const buttonProps: { optional?: React.ReactNode } = {};
+          if (isStepOptional(index)) {
+            buttonProps.optional = <Typography variant="caption">Optional</Typography>;
+          }
+          if (isStepSkipped(index)) {
+            stepProps.completed = false;
+          }
+
+          function isStepComplete(step: number) {
+            return completed.has(step);
+          }
+
+          return (
             <Step key={label}>
-              <StepLabel>{label}</StepLabel>
+              <StepButton
+                onClick={handleStep(index)}
+                completed={isStepComplete(index)}
+                {...buttonProps}
+              >
+                {label}
+              </StepButton>
               <StepContent>
-                <Typography>{getStepContent(index)}</Typography>
-                <div className={classes.actionsContainer}>
-                  <div>
-                    <Button
-                      disabled={activeStep === 0}
-                      onClick={handleBack}
-                      className={classes.button}
-                    >
-                      Back
-                    </Button>
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      onClick={handleNext}
-                      className={classes.button}
-                    >
-                      {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-                    </Button>
-                  </div>
-                </div>
+                <Typography>{getStepContent(activeStep)}</Typography>
               </StepContent>
             </Step>
-          ))}
+          );
+          })}
         </Stepper>
         {activeStep === steps.length && (
-          <Paper square elevation={0} className={classes.resetContainer}>
+          <Paper square elevation={0} className={classes.instructions}>
             <Typography>All steps completed - you&apos;re finished</Typography>
             <Button onClick={handleReset} className={classes.button}>
               Reset
@@ -219,7 +280,8 @@ const ActualGestation = (props:any)=>{
           </Paper>
         )}
       </div>
-      <Grid container justify="center"  className= "Button-Container">
+
+        <Grid container justify="center"  className= "Button-Container">
         <ButtonGroup>
             <Link to={{pathname:`/general/`+_id}} className="Link" >
               <Button  className="HistoryButton" type="submit" variant="contained" color="primary"> History</Button>
